05adc4c11d56bd067c941323d152fbaf4efa80e4
Facu <subiabrewd@gmail.com>
2020-07-04 20:43:44 +0200

    Add memory difference test

--- tests/AppGatiTest.php
+++ tests/AppGatiTest.php
@@ -62,4 +62,40 @@ class AppGatiTest extends TestCase
         $this->assertArrayHasKey('memory', $test2);
         $this->assertArrayHasKey('peak_memory', $test2);
     }
+
+    public function testGetsTimeDifference()
+    {
+        $app = new AppGati;
+
+        $app->step('test1');
+
+        \usleep(100);
+
+        $app->step('test2');
+
+        $time = $app->getTimeDifference('test1', 'test2');
+
+        $this->assertIsNumeric($time);
+        $this->assertLessThanOrEqual(0.1, $time);
+    }
+
+    public function testGetsMemoryDifference()
+    {
+        $app = new AppGati;
+
+        $app->step('test1');
+
+        $i = 0;
+        while ($i <= 1000) {
+            \random_int(0, 1000);
+            $i++;
+        }
+
+        $app->step('test2');
+
+        $memory = $app->getMemoryDifference('test1', 'test2');
+
+        $this->assertIsNumeric($memory);
+        $this->assertGreaterThanOrEqual(1, $memory);
+    }
 }
